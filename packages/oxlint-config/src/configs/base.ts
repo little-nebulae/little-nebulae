013b2469ee045cfe5369b9323ae1a13e2d684cf9
Ryan Luong <lumintriet@gmail.com>
2026-06-25 09:22:51 +0700

    refactor(oxlint-config): make baseConfig use exported options.

--- packages/oxlint-config/src/configs/base.ts
+++ packages/oxlint-config/src/configs/base.ts
@@ -1,29 +1,11 @@
 import { defineConfig } from "oxlint";
 
+import { sharedEnv } from "@/options/env";
 import { sharedPlugins } from "@/options/plugins/native";
+import { sharedRules } from "@/options/rules";
 
 export const baseConfig = defineConfig({
   plugins: sharedPlugins,
-  rules: {
-    curly: "error",
-    "typescript/consistent-type-imports": "error",
-    "typescript/no-import-type-side-effects": "error",
-    "typescript/only-throw-error": [
-      "error",
-      {
-        allow: [
-          {
-            from: "package",
-            name: "Redirect",
-            package: "@tanstack/router-core",
-          },
-          {
-            from: "package",
-            name: "NotFoundError",
-            package: "@tanstack/router-core",
-          },
-        ],
-      },
-    ],
-  },
+  rules: sharedRules,
+  env: sharedEnv,
 });
