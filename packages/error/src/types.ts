a546eee1bb9ecbbe3b677f9d6c88d057911c355b
Ryan Luong <lumintriet@gmail.com>
2026-06-26 10:27:02 +0700

    refactor(error/types): rename to ErrorShape.

--- packages/error/src/types.ts
+++ packages/error/src/types.ts
@@ -3,6 +3,6 @@ import type { z } from "zod";
 import type { createErrorSchema } from "@/factories";
 import type { UnexpectedErrorSchema } from "@/schemas";
 
-export type ErrorType = z.infer<ReturnType<typeof createErrorSchema<string, boolean>>>;
+export type ErrorShape = z.infer<ReturnType<typeof createErrorSchema<string, boolean>>>;
 
 export type UnexpectedError = z.infer<typeof UnexpectedErrorSchema>;
