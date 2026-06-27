8923d15df8811af248f5eb181292fcaf37286412
Ryan Luong <lumintriet@gmail.com>
2026-06-27 19:59:50 +0700

    feat(types/context): add Exception type to Context.

--- packages/types/src/context.ts
+++ packages/types/src/context.ts
@@ -1,3 +1,5 @@
+import type { Exception } from "@little-nebulae/exception";
+
 export type ContextLevel = "error" | "warn" | "info";
 
 export interface Context {
@@ -7,7 +9,7 @@ export interface Context {
     name: string;
     input: unknown;
     output: unknown;
-    exceptions: unknown[];
+    exceptions: Exception[];
   };
   performance: {
     start: number;
