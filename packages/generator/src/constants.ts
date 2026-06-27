2df5a0fed5f324428c70decac9c536d4bca427a7
Ryan Luong <lumintriet@gmail.com>
2026-06-27 18:05:13 +0700

    feat(generator/constants): declare WORKSPACE_TYPE constant.

--- packages/generator/src/constants.ts
+++ packages/generator/src/constants.ts
@@ -1,3 +1,8 @@
+export const WORKSPACE_TYPE = Object.freeze({
+  "SINGLE-PACKAGE": "SINGLE-PACKAGE",
+  "MULTI-PACKAGE": "MULTI-PACKAGE",
+});
+
 export const PACKAGE_TYPE = Object.freeze({
   WEB_APP: "WEB_APP",
   API_SERVER: "API_SERVER",
