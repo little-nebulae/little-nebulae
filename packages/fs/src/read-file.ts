1d4abb371a0e6f03431c2ed5c4242f1dcf6d6f43
Ryan Luong <lumintriet@gmail.com>
2026-06-28 17:23:35 +0700

    feat(fs/read-file): handle fs errors.

--- packages/fs/src/read-file.ts
+++ packages/fs/src/read-file.ts
@@ -4,13 +4,13 @@ import type { Result } from "@little-nebulae/types";
 import { createUnexpectedError } from "@little-nebulae/exception";
 import { resolve } from "node:path";
 
-import type { NoEntryError } from "@/errors/types";
+import type { AccessError, NameTooLongError, NoEntryError } from "@/errors/types";
 
-import { createNoEntryError } from "@/errors/utils";
+import { createAccessError, createNameTooLongError, createNoEntryError } from "@/errors/utils";
 
 export async function readTextFile(
   path: string,
-): Promise<Result<string, NoEntryError | UnexpectedError>> {
+): Promise<Result<string, AccessError | NameTooLongError | NoEntryError | UnexpectedError>> {
   try {
     const file = Bun.file(path);
     const text = await file.text();
@@ -23,15 +23,48 @@ export async function readTextFile(
     if (error instanceof Error && "errno" in error) {
       // oxlint-disable-next-line typescript/no-unsafe-type-assertion
       const errnoException = error as ErrnoException;
-      if (errnoException.code === "ENOENT") {
-        return {
-          ok: false,
-          error: createNoEntryError({
-            message: `The file at ${absolutePath} does not exist.`,
-            cause: errnoException,
-            path: absolutePath,
-          }),
-        };
+      const errnoCode = errnoException.code;
+      if (typeof errnoCode === "string") {
+        switch (errnoCode) {
+          case "EACCES": {
+            return {
+              ok: false,
+              error: createAccessError({
+                message: `The process doesn't have access to ${path}.`,
+                cause: errnoException,
+                path: absolutePath,
+              }),
+            };
+          }
+          case "ENAMETOOLONG": {
+            return {
+              ok: false,
+              error: createNameTooLongError({
+                cause: errnoException,
+                path: absolutePath,
+              }),
+            };
+          }
+          case "ENOENT": {
+            return {
+              ok: false,
+              error: createNoEntryError({
+                message: `The file at ${absolutePath} does not exist.`,
+                cause: errnoException,
+                path: absolutePath,
+              }),
+            };
+          }
+          default: {
+            return {
+              ok: false,
+              error: createUnexpectedError({
+                action: `read the text content of a file at ${absolutePath}`,
+                cause: error,
+              }),
+            };
+          }
+        }
       }
     }
     return {
