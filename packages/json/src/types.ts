f85e3fc06b2f7467d888654304c08e11d7b482cf
Ryan Luong <lumintriet@gmail.com>
2026-06-28 08:25:52 +0700

    feat(json/types): define Jsonifiable type.

--- packages/json/src/types.ts
+++ packages/json/src/types.ts
@@ -2,3 +2,8 @@ export type JsonPrimitive = string | number | boolean | null;
 export type JsonObject = { [K in string]: JsonValue };
 export type JsonArray = JsonValue[] | readonly JsonValue[];
 export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
+
+// oxlint-disable-next-line typescript/consistent-indexed-object-style
+export type JsonifiableObject = { [K in string]?: Jsonifiable } | { toJSON: () => Jsonifiable };
+export type JsonifiableArray = readonly Jsonifiable[];
+export type Jsonifiable = JsonPrimitive | JsonifiableObject | JsonifiableArray;
