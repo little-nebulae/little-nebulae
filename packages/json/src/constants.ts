70eb0387db52282489aec2669b29f97f4e18b394
Ryan Luong <lumintriet@gmail.com>
2026-06-28 09:00:37 +0700

    feat(json/space): define default spacing for JSON data.

---
+++ packages/json/src/constants.ts
@@ -0,0 +1,1 @@
+export const JSON_DATA_DEFAULT_SPACE = 2;
--- packages/json/src/json-stringify.ts
+++ packages/json/src/json-stringify.ts
@@ -2,15 +2,19 @@ import type { UnserializableError } from "@little-nebulae/exception";
 import { createUnserializableError } from "@little-nebulae/exception";
 import type { Ok, Result } from "@little-nebulae/types";
 
+import { JSON_DATA_DEFAULT_SPACE } from "@/constants";
 import type { Jsonifiable } from "@/types";
 
-export function jsonStringify(value: Jsonifiable): Ok<string>;
-export function jsonStringify(value: unknown): Result<string, UnserializableError>;
-export function jsonStringify(value: unknown): Result<string, UnserializableError> {
+export function jsonStringify(value: Jsonifiable, space?: number): Ok<string>;
+export function jsonStringify(value: unknown, space?: number): Result<string, UnserializableError>;
+export function jsonStringify(
+  value: unknown,
+  space: number = JSON_DATA_DEFAULT_SPACE,
+): Result<string, UnserializableError> {
   try {
     return {
       ok: true,
-      data: JSON.stringify(value),
+      data: JSON.stringify(value, null, space),
     };
   } catch (error) {
     return {
