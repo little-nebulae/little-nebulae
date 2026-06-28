4d03727e2e7dcb13d1fe80a2a131d9d2ea1e5917
Ryan Luong <lumintriet@gmail.com>
2026-06-28 16:48:04 +0700

    feat(fs/errors): define retryable field for FileSystemError.

--- packages/fs/src/errors/types.ts
+++ packages/fs/src/errors/types.ts
@@ -1,6 +1,8 @@
 import type { BaseError } from "@little-nebulae/exception";
 
 export interface FileSystemError<C extends string> extends BaseError<C> {
+  // Some errors may be retried, this is just the default
+  retryable: false;
   cause: ErrnoException;
   path: string;
 }
