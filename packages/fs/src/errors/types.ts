ed68f7dce3f98527a828667d6d4adfc1849cf220
Ryan Luong <lumintriet@gmail.com>
2026-06-28 16:45:07 +0700

    feat(fs/errors): define FileSystemError and AccessError types.

--- packages/fs/src/errors/types.ts
+++ packages/fs/src/errors/types.ts
@@ -1,7 +1,9 @@
 import type { BaseError } from "@little-nebulae/exception";
 
-export interface NoEntryError extends BaseError<"NO_ENTRY_ERROR"> {
-  retryable: false;
+export interface FileSystemError<C extends string> extends BaseError<C> {
   cause: ErrnoException;
   path: string;
 }
+
+export type AccessError = FileSystemError<"ACCESS_ERROR">;
+export type NoEntryError = FileSystemError<"NO_ENTRY_ERROR">;
