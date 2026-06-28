89e50a30a5b2337fabaedcbbc79754e6b6a4d54c
Ryan Luong <lumintriet@gmail.com>
2026-06-28 15:37:17 +0700

    feat(pkg/schemas): declare BunLockfileSchema.

--- packages/pkg/src/schemas/bun-lockfile.ts
+++ packages/pkg/src/schemas/bun-lockfile.ts
@@ -1,9 +1,17 @@
 import { z } from "zod";
 
-import { PackageJsonSchema } from "@/schemas/package-json";
+import { SUPPORTED_BUN_LOCKFILE_VERSION } from "@/constants";
+import { BunCatalogSchema, BunCatalogsSchema, PackageJsonSchema } from "@/schemas/package-json";
 
 export const BunLockfileWorkspacesSchema = z
   .object({
     "": PackageJsonSchema,
   })
   .catchall(PackageJsonSchema);
+
+export const BunLockfileSchema = z.object({
+  lockfileVersion: z.literal(SUPPORTED_BUN_LOCKFILE_VERSION),
+  workspaces: BunLockfileWorkspacesSchema,
+  catalog: BunCatalogSchema,
+  catalogs: BunCatalogsSchema,
+});
