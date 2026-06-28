49a2ac9778050f9e7553ab988ec78aeca34a45f7
Ryan Luong <lumintriet@gmail.com>
2026-06-28 20:03:30 +0700

    feat(fs/ops): handle ENOENT in readTextFile function.

--- packages/fs/src/lib/exception/constants.ts
+++ packages/fs/src/lib/exception/constants.ts
@@ -1,6 +1,6 @@
 export const FS_ERRORS = {
   NO_ENTRY: {
-    code: "NO_ENTRY",
+    code: "NO_ENTRY_ERROR",
     errnoCode: "ENOENT",
   },
 } as const;
--- packages/fs/src/read-file.ts
+++ packages/fs/src/read-file.ts
@@ -4,7 +4,13 @@ import type { Result } from "@little-nebulae/types";
 import { createUnexpectedError } from "@little-nebulae/exception";
 import { resolve } from "node:path";
 
-export async function readTextFile(path: string): Promise<Result<string, UnexpectedError>> {
+import type { NoEntryError } from "@/lib/exception/errors";
+
+import { FS_ERRORS } from "@/lib/exception/constants";
+
+export async function readTextFile(
+  path: string,
+): Promise<Result<string, NoEntryError | UnexpectedError>> {
   try {
     const file = Bun.file(path);
     const text = await file.text();
@@ -13,10 +19,27 @@ export async function readTextFile(path: string): Promise<Result<string, Unexpec
       data: text,
     };
   } catch (error) {
+    const absolutePath = resolve(path);
+    if (error instanceof Error && "errno" in error) {
+      // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+      const exception = error as ErrnoException;
+      if (exception.code === FS_ERRORS.NO_ENTRY.errnoCode) {
+        return {
+          ok: false,
+          error: {
+            code: "NO_ENTRY_ERROR",
+            message: `No file exists at ${absolutePath}.`,
+            retryable: false,
+            cause: exception,
+            path: absolutePath,
+          },
+        };
+      }
+    }
     return {
       ok: false,
       error: createUnexpectedError({
-        action: `read the text content of a file at ${resolve(path)}`,
+        action: `read the text content of a file at ${absolutePath}`,
         cause: error,
       }),
     };
