841b1163cbb5d8181e89a0c27cd6f0c58d1946bb
Ryan Luong <lumintriet@gmail.com>
2026-06-28 16:58:22 +0700

    feat(fs/errors): define FileSystemErrorCode type.

--- packages/fs/src/errors/types.ts
+++ packages/fs/src/errors/types.ts
@@ -1,6 +1,8 @@
 import type { BaseError } from "@little-nebulae/exception";
 
-interface FileSystemError<C extends string> extends BaseError<C> {
+export type FileSystemErrorCode = "ACCESS_ERROR" | "NAME_TOO_LONG_ERROR" | "NO_ENTRY_ERROR";
+
+interface FileSystemError<C extends FileSystemErrorCode> extends BaseError<C> {
   // Some errors may be retried, this is just the default
   retryable: false;
   cause: ErrnoException;
