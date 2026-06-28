459cc7415d36821b8e0e53f2ce57034e1fbf57d1
Ryan Luong <lumintriet@gmail.com>
2026-06-28 08:50:03 +0700

    feat(json/stringify): add different overloads for jsonStringify function.

--- packages/json/src/json-stringify.ts
+++ packages/json/src/json-stringify.ts
@@ -1,5 +1,21 @@
+import type { UnserializableError } from "@little-nebulae/exception";
+import { createUnserializableError } from "@little-nebulae/exception";
+import type { Ok, Result } from "@little-nebulae/types";
+
 import type { Jsonifiable } from "@/types";
 
-export function jsonStringify(value: Jsonifiable): string {
-  return JSON.stringify(value);
+export function jsonStringify(value: Jsonifiable): Ok<string>;
+export function jsonStringify(value: unknown): Result<string, UnserializableError>;
+export function jsonStringify(value: unknown): Result<string, UnserializableError> {
+  try {
+    return {
+      ok: true,
+      data: JSON.stringify(value),
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error: createUnserializableError({ format: "JSON", cause: error }),
+    };
+  }
 }
