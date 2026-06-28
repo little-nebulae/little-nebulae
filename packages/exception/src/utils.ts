ec55f907c8ed9097e966f02d32acad10828f27af
Ryan Luong <lumintriet@gmail.com>
2026-06-28 08:38:15 +0700

    feat(exception/utils): define createUnserializableError function.

--- packages/exception/src/utils.ts
+++ packages/exception/src/utils.ts
@@ -1,9 +1,22 @@
-import type { UnexpectedError } from "@/types/errors";
+import type { UnexpectedError, UnserializableError } from "@/types/errors";
 
 export function createUnexpectedError(action: string): UnexpectedError {
   return {
     code: "UNEXPECTED_ERROR",
     message: `An unexpected error was caught while trying to ${action}.`,
     retryable: false,
+  };
+}
+
+export function createUnserializableError({
+  format,
+  cause,
+}: Pick<UnserializableError, "format" | "cause">): UnserializableError {
+  return {
+    code: "UNSERIALIZABLE_ERROR",
+    message: `The given value cannot be serialized to ${format}.`,
+    retryable: false,
+    format,
+    cause,
   };
 }
