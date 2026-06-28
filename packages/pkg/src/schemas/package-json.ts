9030bd7efa2bf80da4b460a175836fe3ca598594
Ryan Luong <lumintriet@gmail.com>
2026-06-28 09:53:25 +0700

    feat(pkg/schemas): declare PackageJsonSchema.

--- packages/pkg/src/schemas/package-json.ts
+++ packages/pkg/src/schemas/package-json.ts
@@ -2,6 +2,20 @@ import { z } from "zod";
 
 export const DependenciesSchema = z.record(z.string(), z.string());
 export const WorkspacesSchema = z.array(z.string());
+export const PackageJsonSchema = z
+  .object({
+    name: z.string(),
+    version: z.string(),
+    private: z.union([z.boolean(), z.literal("true"), z.literal("false")]),
+    type: z.union([z.literal("module"), z.literal("commonjs")]),
+    workspaces: WorkspacesSchema,
+    scripts: z.record(z.string(), z.string()),
+    dependencies: DependenciesSchema,
+    devDependencies: DependenciesSchema,
+    peerDependencies: DependenciesSchema,
+  })
+  .partial();
+export type PackageJson = z.infer<typeof PackageJsonSchema>;
 
 export const BunCatalogSchema = DependenciesSchema;
 export const BunCatalogsSchema = z.record(z.string(), DependenciesSchema);
