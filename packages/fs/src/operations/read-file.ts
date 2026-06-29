352d0d58159d76642f3f82500828ffc72f35075b
Ryan Luong <lumintriet@gmail.com>
2026-06-29 09:19:22 +0700

    feat(fs/ops): rewrite readTextFile to use the new version of the exception package.

--- packages/fs/src/operations/read-file.ts
+++ packages/fs/src/operations/read-file.ts
@@ -1,16 +1,14 @@
-import type { UnexpectedError } from "@little-nebulae/exception";
 import type { Result } from "@little-nebulae/types";
 
-import { createUnexpectedError } from "@little-nebulae/exception";
+import { UnexpectedError } from "@little-nebulae/exception";
 import { resolve } from "node:path";
 
-import type { NoEntryError } from "@/lib/exception/errors";
-
-import { FS_ERRORS } from "@/lib/exception/constants";
+import { FsNoEntryError } from "@/lib/exception/classes/error";
+import { FS_ERRNO_CODES } from "@/lib/exception/constants";
 
 export async function readTextFile(
   path: string,
-): Promise<Result<string, NoEntryError | UnexpectedError>> {
+): Promise<Result<string, FsNoEntryError | UnexpectedError>> {
   try {
     const file = Bun.file(path);
     const text = await file.text();
@@ -23,23 +21,21 @@ export async function readTextFile(
     if (error instanceof Error && "errno" in error) {
       // oxlint-disable-next-line typescript/no-unsafe-type-assertion
       const exception = error as ErrnoException;
-      if (exception.code === FS_ERRORS.NO_ENTRY.errnoCode) {
+      if (exception.code === FS_ERRNO_CODES.FS_NO_ENTRY_ERROR) {
         return {
           ok: false,
-          error: {
-            code: "NO_ENTRY_ERROR",
+          error: new FsNoEntryError({
             message: `Failed to read file at ${absolutePath} because it doesn't exist.`,
-            retryable: false,
             cause: exception,
             path: absolutePath,
-          },
+          }),
         };
       }
     }
     return {
       ok: false,
-      error: createUnexpectedError({
-        action: `read the text content of a file at ${absolutePath}`,
+      error: new UnexpectedError({
+        action: `read file at ${absolutePath}`,
         cause: error,
       }),
     };
