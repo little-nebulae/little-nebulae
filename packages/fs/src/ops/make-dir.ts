fbb6351b9ad7ff09d95301b09aa1ae14b11aacd9
Ryan Luong <lumintriet@gmail.com>
2026-06-29 18:58:24 +0700

    feat(fs/ops): handle ENOENT error in makeDir function.

--- packages/fs/src/ops/make-dir.ts
+++ packages/fs/src/ops/make-dir.ts
@@ -3,6 +3,10 @@ import type { MakeDirectoryOptions } from "node:fs";
 
 import { UnexpectedError } from "@little-nebulae/exception";
 import { mkdir } from "node:fs/promises";
+import { resolve, dirname } from "node:path";
+
+import { FsNoEntryError } from "@/lib/exception/classes/error";
+import { FS_ERRNO_CODES } from "@/lib/exception/constants";
 
 export async function makeDir(
   path: string,
@@ -15,7 +19,7 @@ export async function makeDir(
 export async function makeDir(
   path: string,
   options: MakeDirectoryOptions = {},
-): Promise<Result<string | null, UnexpectedError>> {
+): Promise<Result<string | null, FsNoEntryError | UnexpectedError>> {
   try {
     const createdDirPath = await mkdir(path, options);
     return {
@@ -23,10 +27,25 @@ export async function makeDir(
       data: createdDirPath ?? null,
     };
   } catch (error) {
+    const absolutePath = resolve(path);
+    if (error instanceof Error && "errno" in error) {
+      // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+      const exception = error as ErrnoException;
+      if (exception.code === FS_ERRNO_CODES.FS_NO_ENTRY_ERROR) {
+        return {
+          ok: false,
+          error: new FsNoEntryError({
+            message: `Failed to make a directory at ${absolutePath} because a directory component in ${dirname(absolutePath)} does not exist or is a dangling symbolic link.`,
+            cause: exception,
+            path: absolutePath,
+          }),
+        };
+      }
+    }
     return {
       ok: false,
       error: new UnexpectedError({
-        action: `make a directory at ${path}`,
+        action: `make a directory at ${absolutePath}`,
         cause: error,
       }),
     };
