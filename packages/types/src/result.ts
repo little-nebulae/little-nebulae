68405505ffbcaed63bb827b34965151de89db789
Ryan Luong <lumintriet@gmail.com>
2026-06-29 08:41:35 +0700

    refactor(types/result): extends AppError from new exception package version.

--- packages/types/src/result.ts
+++ packages/types/src/result.ts
@@ -1,13 +1,13 @@
-import type { BaseError } from "@little-nebulae/exception";
+import type { AppError } from "@little-nebulae/exception";
 
 export interface Ok<D> {
   ok: true;
   data: D;
 }
 
-export interface Err<E extends BaseError<string>> {
+export interface Err<E extends AppError<string>> {
   ok: false;
   error: E;
 }
 
-export type Result<D, E extends BaseError<string>> = Ok<D> | Err<E>;
+export type Result<D, E extends AppError<string>> = Ok<D> | Err<E>;
