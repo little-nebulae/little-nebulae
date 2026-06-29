9ec6f1b410b47944e03a8862bbe3ab7ac5f29506
Ryan Luong <lumintriet@gmail.com>
2026-06-29 08:36:45 +0700

    feat(exception/entry): define new exports.

--- packages/exception/src/index.ts
+++ packages/exception/src/index.ts
@@ -1,5 +1,3 @@
-export type * from "@/types/errors";
-export type * from "@/types/warnings";
-export type * from "@/types/exceptions";
+export * from "serialize-error";
 
-export * from "@/utils";
+export * from "@/classes/error";
