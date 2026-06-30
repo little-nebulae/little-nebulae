4c8288af3f2b2ceb246bb090be36681fb012ae34
Ryan Luong <lumintriet@gmail.com>
2026-06-30 11:13:29 +0700

    build(pkg/config): configure tsdown, add build script and exports.

---
+++ packages/pkg/tsdown.config.ts
@@ -0,0 +1,9 @@
+import { libConfig } from "@little-nebulae/tsdown-config";
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  ...libConfig,
+  deps: {
+    neverBundle: ["zod"],
+  },
+});
