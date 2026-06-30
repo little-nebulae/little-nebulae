864b2c784902d4468dea53ab31a8867d69d387a1
Ryan Luong <lumintriet@gmail.com>
2026-06-30 08:58:40 +0700

    feat(fs/entry): define lib's entry point.

---
+++ packages/fs/src/index.ts
@@ -0,0 +1,12 @@
+export * from "@/lib/exception/constants";
+export * from "@/lib/exception/classes/error";
+
+export * from "@/ops/dir/read";
+export * from "@/ops/dir/make";
+export * from "@/ops/dir/make-temp";
+export * from "@/ops/file/read";
+export * from "@/ops/file/write";
+export * from "@/ops/file/delete";
+export * from "@/ops/remove";
+
+export * from "@/utils/get-fs-friendly-date-string";
