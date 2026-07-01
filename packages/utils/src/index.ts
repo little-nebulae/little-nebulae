da17de09a798a0da6db37197b08126e26ca6d861
Ryan Luong <lumintriet@gmail.com>
2026-07-01 07:58:31 +0700

    feat(utils): define capitalize function.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,4 @@
-// oxlint-disable unicorn/no-empty-file
+export function capitalize(str: string): string {
+  // oxlint-disable-next-line no-magic-numbers
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
