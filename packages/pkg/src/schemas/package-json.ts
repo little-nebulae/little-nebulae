567d5eba1fb8b92a34e24fc00decf180eb4ffe43
Ryan Luong <lumintriet@gmail.com>
2026-07-01 08:31:48 +0700

    feat(pkg/schemas): export BunWorkspaces schema and its inferred type.

--- packages/pkg/src/schemas/package-json.ts
+++ packages/pkg/src/schemas/package-json.ts
@@ -21,21 +21,23 @@ export const BunCatalogSchema = DependenciesSchema;
 export type BunCatalog = z.infer<typeof BunCatalogSchema>;
 export const BunCatalogsSchema = z.record(z.string(), DependenciesSchema);
 export type BunCatalogs = z.infer<typeof BunCatalogsSchema>;
+export const BunWorkspacesSchema = z.union([
+  WorkspacesSchema,
+  z
+    .object({
+      packages: WorkspacesSchema,
+      catalog: BunCatalogSchema,
+      catalogs: BunCatalogsSchema,
+    })
+    .partial(),
+]);
+export type BunWorkspaces = z.infer<typeof BunWorkspacesSchema>;
 export const BunPackageJsonSchema = z
   .object({
     ...PackageJsonSchema.shape,
     catalog: BunCatalogSchema,
     catalogs: BunCatalogsSchema,
-    workspaces: z.union([
-      WorkspacesSchema,
-      z
-        .object({
-          packages: WorkspacesSchema,
-          catalog: BunCatalogSchema,
-          catalogs: BunCatalogsSchema,
-        })
-        .partial(),
-    ]),
+    workspaces: BunWorkspacesSchema,
   })
   .partial();
 export type BunPackageJson = z.infer<typeof BunPackageJsonSchema>;
